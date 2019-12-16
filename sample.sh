ed2da7b1be4dc2d27522e0be0d96688b104e0d43
hvinett <vineeth@vineeth.fareast.corp.microsoft.com>
2019-12-16 18:14:50 +0530

    added install chrome driver

---
+++ sample.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ];
+  then echo "Please run as root (with sudo command)"
+  exit
+fi
+
+if [ "${VERSION}" == "x86_64:" ]; then
+	CHR_DRIVER="https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux32.zip"
+else
+	CHR_DRIVER="https://chromedriver.storage.googleapis.com/2.38/chromedriver_linux64.zip"
+fi 
+
+echo $CHR_DRIVER
+
+TEMP_PATH=$PWD
+mkdir $TEMP_PATH && pushd $TEMP_PATH
+wget $CHR_DRIVER -P $TEMP_PATH
+
+#sudo apt-get install -f 
+sudo apt install unzip
+unzip ${TEMP_PATH}/chromedriver_linux64.zip
+
+sudo chown root:root ${TEMP_PATH}
+sudo chmod 0755 ${TEMP_PATH}
+
+
+
